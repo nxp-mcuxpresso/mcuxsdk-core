577e990f2a3e5ebe4a379b77266b4d3206e7d1fc
Michal Princ <michal.princ@nxp.com>
2024-12-10 12:31:07 +0100

    [MCUX-74447][ARM] Correct the CM33_no_fpu MDK LD flag
    - When the core is CM33 without FPU, the LD flag for MDK should be "--fpu=SoftVFP"
    
    Signed-off-by: Michal Princ <michal.princ@nxp.com>

--- arch/arm/cortexm/core_no_fpu.cmake
+++ arch/arm/cortexm/core_no_fpu.cmake
@@ -14,6 +14,7 @@ if (CONFIG_MCUX_PRJSEG_config.arm.core.fpu.no_fpu)
         AS "-mfpu=none"
         CC "-mfpu=none"
         CX "-mfpu=none"
+        LD "--fpu=SoftVFP"
     )
 
     mcux_add_armgcc_mcux_configuration(
