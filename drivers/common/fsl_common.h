7dabea6040cbe7f1c23a8d1a690edb2c08cc4be7
Wenbin Song <wenbin.song@nxp.com>
2025-01-04 00:31:46 +0800

    [MCUX-75552]: Add hiperface driver
    Signed-off-by: Wenbin Song <wenbin.song@nxp.com>

--- drivers/common/fsl_common.h
+++ drivers/common/fsl_common.h
@@ -209,6 +209,7 @@ enum _status_groups
     kStatusGroup_AON_POWER      = 169, /*!< Group number for AON_POWER status codes. */
     kStatusGroup_AON_COMMON     = 170, /*!< Group number for AON_COMMON status codes. */
     kStatusGroup_ENDAT3     	= 171, /*!< Group number for ENDAT3 status codes. */
+    kStatusGroup_HIPERFACE      = 172, /*!< Group number for HIPERFACE status codes. */
 };
 
 /*! \public
