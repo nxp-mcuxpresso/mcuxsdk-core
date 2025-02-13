22e31e208c992fd3ea2ba8f06b43e678c5031ae9
Holt.Sun <holt.sun@nxp.com>
2025-02-13 12:43:21 +0800

    MCUX-76196 Update _read_tool_versions function to add process for failing to read tool and toolchain data from tool.yml.
    When build system fails to read tool.yml, then build system will include default_tool_version.cmake to get the backup tools and toolchains versions for later validation.
    
    Signed-off-by: Holt.Sun <holt.sun@nxp.com>

--- cmake/extension/misc_function.cmake
+++ cmake/extension/misc_function.cmake
@@ -3,13 +3,25 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 function(_read_tool_versions read_tool_version_py)
+    set(TOOL_YML ${SdkRootDirPath}/tool.yml)
+    set(TOOL_YML_SCHEMA ${SdkRootDirPath}/mcusdk/script/data_schema/tool.json)
+    set(DEFAULT_TOOL_VERSION_CMAKE ${SdkRootDirPath}/cmake/extension/default_tool_version.cmake)
     # Execute the Python script and capture the output
     execute_process(
         COMMAND ${CMAKE_COMMAND} -E env python ${read_tool_version_py}
         OUTPUT_VARIABLE tool_versions_output
         OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE result
     )
 
+    # Check if the command ran successfully
+    if(NOT result EQUAL 0)
+        message("Warning: Fail to validate ${TOOL_YML} against ${TOOL_YML_SCHEMA}. Failed reasons are: ${tool_versions_output}. ${TOOL_YML} data is used by build system to validate installed tools and toolchain versions, please fix data issue in tool.yml.")
+        message("Warning: Build system will use designated versions in ${DEFAULT_TOOL_VERSION_CMAKE} to do installed tools and toolchain version validation.")
+        include(${DEFAULT_TOOL_VERSION_CMAKE})
+        return()
+    endif()
+    
     # replace \n to ; in tool_versions_output
     string(REPLACE "\n" ";" tool_versions_output "${tool_versions_output}")
 
