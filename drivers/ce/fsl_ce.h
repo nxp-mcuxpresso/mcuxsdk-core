3a15bbeba5c6bcd9b3acd83dcde9851e27e8468d
Allen Zhang <chunfeng.zhang@nxp.com>
2025-03-17 14:53:12 +0800

    [MCUX-77460] Fixed CE driver component detected using SDK_components.xml was not detected from source code
    - Move the FSL_COMPONENT_ID from .h to .c file
    
    Signed-off-by: AllenZhang <chunfeng.zhang@nxp.com>

--- drivers/ce/fsl_ce.h
+++ drivers/ce/fsl_ce.h
@@ -27,11 +27,6 @@ Functional API definitions for ARM drivers for CE
 #define FSL_CE_DRIVER_VERSION (MAKE_VERSION(2, 1, 0))
 /*! @} */
 
-/* Component ID definition, used by tools. */
-#ifndef FSL_COMPONENT_ID
-#define FSL_COMPONENT_ID "platform.drivers.ce"
-#endif
-
 /*!
  * @brief Structure for CE copy image to destination address
  *
