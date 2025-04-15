f2c8169c1d309ee7a13a3efcdad5d4abe244bb7c
Ruijia Wang <ruijia.wang@nxp.com>
2025-04-15 13:52:06 +0800

    [MCUX-78507] Fixed QSPI driver MSG issue
    [Status]
    - Fixed QPSI driver CERT INT31-C MSG issues.
    
    Signed-off-by: Ruijia Wang <ruijia.wang@nxp.com>

--- drivers/qspi/socs/mcxe31b/fsl_qspi_soc.c
+++ drivers/qspi/socs/mcxe31b/fsl_qspi_soc.c
@@ -6,6 +6,15 @@
 
 #include "fsl_qspi_soc.h"
 
+/*! brief Defines the QuadSPI SOCCR mask field. */
+#define QuadSPI_SOCCR_OBE_PULL_TIMING_RELAX_MASK             (1U << 0)
+#define QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_INPUT_MASK         (1U << 1)
+#define QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_OUTPUT_MASK        (1U << 2)
+#define QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_DRIVE_MASK         (1U << 3)
+#define QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_PULL_ENABLE_MASK   (1U << 4)
+#define QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_PULLUP_ENABLE_MASK (1U << 5)
+#define QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_SLEW_RATE_MASK     (1U << 6)
+
 /*!
  * brief Set QuadSPI Soc specific configuration.
  * note Should call it when QuadSPI is disabled.
@@ -16,12 +25,16 @@
  */
 status_t QSPI_SocConfigure(QuadSPI_Type *base, qspi_soc_config_t *config)
 {
-    base->SOCCR = (uint32_t)config->obePullTimingRelax | ((uint32_t)config->sckDummyPadInputEnable << 1U) | ((uint32_t)config->sckDummyPadOutputEnable << 2U) |
-                  ((uint32_t)config->sckDummyPadDriveEnable << 3U) | ((uint32_t)config->sckDummyPadPullEnable << 4U) | ((uint32_t)config->sckDummyPadPullupEnable << 5U) |
-                  ((uint32_t)config->sckDummyPadSlewRateEnable << 6U);
-    
+    base->SOCCR = (config->obePullTimingRelax ? QuadSPI_SOCCR_OBE_PULL_TIMING_RELAX_MASK : 0U) |
+                  (config->sckDummyPadInputEnable ? QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_INPUT_MASK : 0U) |
+                  (config->sckDummyPadOutputEnable ? QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_OUTPUT_MASK : 0U) |
+                  (config->sckDummyPadDriveEnable ? QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_DRIVE_MASK : 0U) |
+                  (config->sckDummyPadPullEnable ? QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_PULL_ENABLE_MASK : 0U) |
+                  (config->sckDummyPadPullupEnable ? QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_PULLUP_ENABLE_MASK : 0U) |
+                  (config->sckDummyPadSlewRateEnable ? QuadSPI_SOCCR_QSPIA_SCK_DUMMY_PAD_SLEW_RATE_MASK : 0U);
+
     base->MCR &= ~QuadSPI_MCR_DQS_FA_SEL_MASK;
-    base->MCR |= QuadSPI_MCR_DQS_FA_SEL(config->dqsClkPadLoopEnable);
+    base->MCR |= QuadSPI_MCR_DQS_FA_SEL((config->dqsClkPadLoopEnable ? 0x1U : 0U));
 
     return kStatus_Success;
 }
--- drivers/qspi/socs/mcxe31b/fsl_qspi_soc.h
+++ drivers/qspi/socs/mcxe31b/fsl_qspi_soc.h
@@ -22,14 +22,22 @@
 /*! @brief Defines the QuadSPI Soc specific configuration. */
 typedef struct _qspi_soc_config
 {
-    bool obePullTimingRelax : 1; /*!< Enables the timing relaxation by pulling obe for pad 1 for half cycle, if 0 then enabled else disabled. */
-    bool sckDummyPadInputEnable : 1; /*!< ibe of QSPIA_SCK_DUMMY pad. 0: Disable input reciever 1: Enable input reciever. */
-    bool sckDummyPadOutputEnable : 1; /*!< obe of QSPIA_SCK_DUMMY pad. 0: Disable output driver 1: Enable output driver. */
-    bool sckDummyPadDriveEnable : 1; /*!< dse of QSPIA_SCK_DUMMY pad. 0: Disable drive strength 1: Enable drive strength. */
-    bool sckDummyPadPullEnable : 1; /*!< pue of QSPIA_SCK_DUMMY pad. 0: Disable internal pullup or pulldown resistor 1: Enable internal pullup or pulldown resistor. */
-    bool sckDummyPadPullupEnable : 1; /*!< pus of QSPIA_SCK_DUMMY pad. 0: Enable internal pulldown resistor if pue is set 1: Enable internal pullup resistor if pue is set. */
-    bool sckDummyPadSlewRateEnable : 1; /*!< sre of QSPIA_SCK_DUMMY pad. 0: Disable slew rate control 1: Enable slew rate control. */
-    bool dqsClkPadLoopEnable : 1;
+    bool obePullTimingRelax : 1; /*!< Enables the timing relaxation by pulling obe for pad 1 for half cycle, if 0 then
+                                    enabled else disabled. */
+    bool sckDummyPadInputEnable : 1;  /*!< ibe of QSPIA_SCK_DUMMY pad. 0: Disable input reciever 1: Enable input
+                                         reciever. */
+    bool sckDummyPadOutputEnable : 1; /*!< obe of QSPIA_SCK_DUMMY pad. 0: Disable output driver 1: Enable output driver.
+                                       */
+    bool sckDummyPadDriveEnable : 1;  /*!< dse of QSPIA_SCK_DUMMY pad. 0: Disable drive strength 1: Enable drive
+                                         strength. */
+    bool sckDummyPadPullEnable : 1; /*!< pue of QSPIA_SCK_DUMMY pad. 0: Disable internal pullup or pulldown resistor 1:
+                                       Enable internal pullup or pulldown resistor. */
+    bool sckDummyPadPullupEnable : 1;   /*!< pus of QSPIA_SCK_DUMMY pad. 0: Enable internal pulldown resistor if pue is
+                                           set 1: Enable internal pullup resistor if pue is set. */
+    bool sckDummyPadSlewRateEnable : 1; /*!< sre of QSPIA_SCK_DUMMY pad. 0: Disable slew rate control 1: Enable slew
+                                           rate control. */
+    bool dqsClkPadLoopEnable : 1; /*!< Enables the internal clock which is loop-backed from the dummy internal pad to
+                                     compensate data pad delays. */
 } qspi_soc_config_t;
 
 #if defined(__cplusplus)
