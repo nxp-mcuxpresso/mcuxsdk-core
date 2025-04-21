0d8330f5c247e53d2c4bf96657a622d142038bf6
Yangbo Lu <yangbo.lu@nxp.com>
2025-04-21 11:27:26 +0800

    [MCUX-78535] drivers: netc: support return value of phywrapper APIs
    Supported return value of phywrapper APIs.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- drivers/netc/fsl_netc_phy_wrapper.c
+++ drivers/netc/fsl_netc_phy_wrapper.c
@@ -6,8 +6,7 @@
 
 #include "fsl_netc_phy_wrapper.h"
 
-void NETC_PHYWriteRegBits(
-    netc_mdio_handle_t *handle, bool pcs, volatile uint16_t *reg, uint16_t mask, uint16_t val)
+status_t NETC_PHYWriteRegBits(netc_mdio_handle_t *handle, bool pcs, volatile uint16_t *reg, uint16_t mask, uint16_t val)
 {
     uint8_t portAddr = pcs ? 0x0U : 0x10U;
     uint8_t devAddr  = getPhyDev(reg);
@@ -16,7 +15,10 @@ void NETC_PHYWriteRegBits(
     status_t status;
 
     status = NETC_MDIOC45Read(handle, portAddr, devAddr, regAddr, &regValue);
-    assert(status == kStatus_Success);
+    if (status != kStatus_Success)
+    {
+        return status;
+    }
 
     regValue &= ~mask;
     if (val != 0U)
@@ -24,23 +26,14 @@ void NETC_PHYWriteRegBits(
         regValue |= val;
     }
 
-    status = NETC_MDIOC45Write(handle, portAddr, devAddr, regAddr, regValue);
-    if (status != kStatus_Success)
-    {
-        assert(false);
-    }
+    return NETC_MDIOC45Write(handle, portAddr, devAddr, regAddr, regValue);
 }
 
-void NETC_PHYReadReg(netc_mdio_handle_t *handle, bool pcs, const volatile uint16_t *reg, uint16_t *val)
+status_t NETC_PHYReadReg(netc_mdio_handle_t *handle, bool pcs, const volatile uint16_t *reg, uint16_t *val)
 {
     uint8_t portAddr = pcs ? 0x0U : 0x10U;
     uint8_t devAddr  = getPhyDev(reg);
     uint16_t regAddr = getPhyReg(reg);
-    status_t status;
 
-    status = NETC_MDIOC45Read(handle, portAddr, devAddr, regAddr, val);
-    if (status != kStatus_Success)
-    {
-        assert(false);
-    }
+    return NETC_MDIOC45Read(handle, portAddr, devAddr, regAddr, val);
 }
