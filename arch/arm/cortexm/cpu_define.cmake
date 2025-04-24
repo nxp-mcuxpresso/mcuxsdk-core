a03a3f5412a4f4439f1ed25bf553f914a3677e0d
Eric Sun <jian.sun@nxp.com>
2025-04-24 19:02:50 +0800

    [MCUX-78990] Add "_SERIES" macro definition in project settings

--- arch/arm/cortexm/cpu_define.cmake
+++ arch/arm/cortexm/cpu_define.cmake
@@ -4,8 +4,8 @@
 
 if (CONFIG_MCUX_PRJSEG_config.device_core.define)
     mcux_add_configuration(
-        AS "-DCPU_${CONFIG_MCUX_HW_DEVICE_PART}${core_id_suffix_name}"
-        CC "-DCPU_${CONFIG_MCUX_HW_DEVICE_PART}${core_id_suffix_name}"
-        CX "-DCPU_${CONFIG_MCUX_HW_DEVICE_PART}${core_id_suffix_name}"
+        AS "-DCPU_${CONFIG_MCUX_HW_DEVICE_PART}${core_id_suffix_name} -D${device}${core_id_suffix_name}_SERIES"
+        CC "-DCPU_${CONFIG_MCUX_HW_DEVICE_PART}${core_id_suffix_name} -D${device}${core_id_suffix_name}_SERIES"
+        CX "-DCPU_${CONFIG_MCUX_HW_DEVICE_PART}${core_id_suffix_name} -D${device}${core_id_suffix_name}_SERIES"
     )
 endif()
