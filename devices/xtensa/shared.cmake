4073e0be110cc833d70215852f874283d6a82272
Feng Wei <wei.feng@nxp.com>
2025-04-27 11:28:11 +0800

    [MCUX-75993] Improve Kconfig
    - Removed unused RTE file from ezhv and xtensa
    - Made Kconfig menu wording concise and lucid
    
    Signed-off-by: Feng Wei <wei.feng@nxp.com>

--- devices/xtensa/shared.cmake
+++ devices/xtensa/shared.cmake
@@ -1,19 +1,5 @@
 # core related
 include(${SdkRootDirPath}/arch/xtensa/core/core_${CONFIG_MCUX_HW_CORE}.cmake)
 
-if(CONFIG_MCUX_COMPONENT_device.RTE)
-    mcux_add_source(
-      BASE_PATH ${SdkRootDirPath}/${device_root}/${soc_portfolio}/${soc_series}/${device}/template
-      SOURCES RTE_Device.h
-      CONFIG true
-    )
-
-    mcux_add_include(
-      BASE_PATH ${SdkRootDirPath}/${device_root}/${soc_portfolio}/${soc_series}/${device}/template
-      PROJECT_BASE_PATH board
-      INCLUDES .
-    )
-endif()
-
 # build target related
 include(${SdkRootDirPath}/arch/xtensa/target/targets.cmake)
