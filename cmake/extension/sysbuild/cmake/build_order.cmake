5dbabaf337a4ba417bc0f574bf0d0274a36e813f
Felix Wang <fei.wang_3@nxp.com>
2025-08-07 11:49:42 +0530

    [SDKGEN-3487]build_order setting improvement
    When setting build_order in domains.yaml, if there is only
    one project, or there is no build order dependency, just
    set them in build_order as cmake record order.
    
    Signed-off-by: Felix Wang <fei.wang_3@nxp.com>

--- cmake/extension/sysbuild/cmake/build_order.cmake
+++ cmake/extension/sysbuild/cmake/build_order.cmake
@@ -11,16 +11,22 @@ foreach(target ${IMAGES})
   endif()
 endforeach()
 
-execute_process(
-  COMMAND ${PYTHON_EXECUTABLE} ${SdkRootDirPath}/cmake/extension/sysbuild/scripts/build_order.py "${build_dependecy}" -o ${CMAKE_BINARY_DIR}/build_order.yaml
-  RESULT_VARIABLE EXEC_RESULT
-)
+if(build_dependecy)
+  execute_process(
+    COMMAND ${PYTHON_EXECUTABLE} ${SdkRootDirPath}/cmake/extension/sysbuild/scripts/build_order.py "${build_dependecy}" -o ${CMAKE_BINARY_DIR}/build_order.yaml
+    RESULT_VARIABLE EXEC_RESULT
+  )
 
-if((EXEC_RESULT STREQUAL 0) AND (EXISTS "${CMAKE_BINARY_DIR}/build_order.yaml"))
-  # Read build order setting to BUILD_ORDER_CONTENT 
-  file(READ "${CMAKE_BINARY_DIR}/build_order.yaml" BUILD_ORDER_CONTENT)
+  if((EXEC_RESULT STREQUAL 0) AND (EXISTS "${CMAKE_BINARY_DIR}/build_order.yaml"))
+    # Read build order setting to BUILD_ORDER_CONTENT 
+    file(READ "${CMAKE_BINARY_DIR}/build_order.yaml" BUILD_ORDER_CONTENT)
+  else()
+    message(SEND_ERROR "Error occured when analysising building order.")
+  endif()
 else()
-  message(SEND_ERROR "Error occured when analysising building order.")
+  # if no build dependency, build it as record order
+  set(BUILD_ORDER_CONTENT "build_order:")
+  foreach(image ${IMAGES})
+    set(BUILD_ORDER_CONTENT "${BUILD_ORDER_CONTENT}\n  - ${image}")
+  endforeach()
 endif()
-
-
