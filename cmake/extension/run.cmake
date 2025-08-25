2badd23175eb5b6b2344c5b00075e7b57f62d43a
Jiafei Pan <Jiafei.Pan@nxp.com>
2025-08-25 15:18:01 +0800

    [nxp toup] cmake: extension: add board runner suport for customer board
    Add board runner support for customer board.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- cmake/extension/run.cmake
+++ cmake/extension/run.cmake
@@ -1,10 +1,13 @@
-# Copyright 2024 NXP
+# Copyright 2024-2025 NXP
 #
 # SPDX-License-Identifier: BSD-3-Clause
 add_custom_target(runners_yaml_props_target)
 
 include(${SdkRootDirPath}/cmake/extension/flash/extensions.cmake)
-
-include(${SdkRootDirPath}/${board_root}/${board}/board_runner.cmake OPTIONAL)
+if (DEFINED CUSTOM_BOARD_ROOT AND NOT CUSTOM_BOARD_ROOT STREQUAL "")
+    include(${CUSTOM_BOARD_ROOT}/${board}/board_runner.cmake OPTIONAL)
+else()
+    include(${SdkRootDirPath}/${board_root}/${board}/board_runner.cmake OPTIONAL)
+endif()
 
 mcux_add_cmakelists(${SdkRootDirPath}/cmake/extension/flash)
