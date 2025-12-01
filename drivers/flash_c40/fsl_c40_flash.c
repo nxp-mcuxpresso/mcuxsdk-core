a21905b3ef212616da60755d3d27aedeb542d71c
Petr Buchta <petr.buchta@nxp.com>
2025-12-01 10:25:34 +0100

    [ZEP-3844][flash_c40] Fix address increment in flash write
    Signed-off-by: Petr Buchta <petr.buchta@nxp.com>

--- drivers/flash_c40/fsl_c40_flash.c
+++ drivers/flash_c40/fsl_c40_flash.c
@@ -839,7 +839,7 @@ status_t FLASH_Program(flash_config_t *config, uint32_t start, uint32_t *src, ui
 
         chunkSize = (sizeLeft > bytesToAlignedPage) ? bytesToAlignedPage : sizeLeft;
 
-        ret = C40_Write(start, lengthInBytes, src8, C40_DEFAULT_CORE_DOMAIN_ID);
+        ret = C40_Write(addr, chunkSize, src8, C40_DEFAULT_CORE_DOMAIN_ID);
         if (ret != kStatus_FLASH_Success)
         {
             return ret;
@@ -847,16 +847,16 @@ status_t FLASH_Program(flash_config_t *config, uint32_t start, uint32_t *src, ui
 
         sizeLeft -= chunkSize;
         src8     += chunkSize;
+        addr     += chunkSize;
     }
 
     /* continue with aligned writes */
 
     while (sizeLeft > 0)
     {
-
         chunkSize = (sizeLeft > C40_WRITE_SIZE_MAX) ? C40_WRITE_SIZE_MAX : sizeLeft;
 
-        ret = C40_Write(start, lengthInBytes, src8, C40_DEFAULT_CORE_DOMAIN_ID);
+        ret = C40_Write(addr, chunkSize, src8, C40_DEFAULT_CORE_DOMAIN_ID);
         if (ret != kStatus_FLASH_Success)
         {
             return ret;
@@ -864,6 +864,7 @@ status_t FLASH_Program(flash_config_t *config, uint32_t start, uint32_t *src, ui
 
         sizeLeft -= chunkSize;
         src8     += chunkSize;
+        addr     += chunkSize;
     }
 
     return ret;
