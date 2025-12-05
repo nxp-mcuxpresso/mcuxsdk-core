05c22cf90322a0ae87726227b95243337ab5f9e4
Holt Sun <holt.sun@nxp.com>
2025-12-05 11:24:03 +0800

    SDKGEN-3525 build system: fix runner include path for custom boards
    Ensure board_runner.cmake is located and included correctly when using
    custom board ports, so `west run` and related runner logic work for
    non-default boards.
    
    Signed-off-by: Holt Sun <holt.sun@nxp.com>

--- cmake/extension/run.cmake
+++ cmake/extension/run.cmake
@@ -5,6 +5,10 @@ add_custom_target(runners_yaml_props_target)
 
 include(${SdkRootDirPath}/cmake/extension/flash/extensions.cmake)
 
+if (DEFINED CUSTOM_BOARD_ROOT AND NOT CUSTOM_BOARD_ROOT STREQUAL "")
+include(${CUSTOM_BOARD_ROOT}/${board}/board_runner.cmake OPTIONAL)
+else()
 include(${SdkRootDirPath}/${board_root}/${board}/board_runner.cmake OPTIONAL)
+endif()
 
 mcux_add_cmakelists(${SdkRootDirPath}/cmake/extension/flash)
