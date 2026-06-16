c0dd0f4e43f6f2a1b557bdfd2616fc2d6c4a85d2
Ruijia Wang <ruijia.wang@nxp.com>
2026-06-16 20:44:28 +0800

    [MCUX-88312][QSPI] Fix CERT INT30-C/INT31-C MSG findings
    Guard srcClock_Hz against unsigned wraparound (INT30-C) and convert
    enableDQSClkInverse to 0U/1U before the SOCCR field macro (INT31-C).
    
    Signed-off-by: Ruijia Wang <ruijia.wang@nxp.com>

--- drivers/qspi/fsl_qspi.c
+++ drivers/qspi/fsl_qspi.c
@@ -139,6 +139,7 @@ void QSPI_Init(QuadSPI_Type *base, qspi_config_t *config, uint32_t srcClock_Hz)
     /* To avoid the configured baudrate exceeds the expected baudrate value, which may possibly put the
     QSPI work under unsupported frequency, set the divider higher when there is reminder, use ceiling
     operation, ceiling(a/b) = (a-1)/b + 1. */
+    assert(srcClock_Hz != 0U);
     val |= QuadSPI_MCR_SCLKCFG((srcClock_Hz - 1U) / config->baudRate);
     base->MCR = val;
 #endif /* FSL_FEATURE_QSPI_CLOCK_CONTROL_EXTERNAL */
@@ -348,7 +349,7 @@ void QSPI_SetDqsConfig(QuadSPI_Type *base, qspi_dqs_config_t *config)
             break;
     }
 
-    soccrVal |= (QuadSPI_SOCCR_DQS_INV_EN(config->enableDQSClkInverse) |
+    soccrVal |= (QuadSPI_SOCCR_DQS_INV_EN(config->enableDQSClkInverse ? 1U : 0U) |
                  QuadSPI_SOCCR_DQS_IFA_DELAY_CHAIN_SEL(config->portADelayTapNum)
 #if defined(QuadSPI_SOCCR_DQS_IFB_DELAY_CHAIN_SEL_MASK)
                  | QuadSPI_SOCCR_DQS_IFB_DELAY_CHAIN_SEL(config->portBDelayTapNum)
